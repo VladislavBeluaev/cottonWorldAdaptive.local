d00f16a8b0e8859424529330410f90496f9396b0
= <=>
2019-05-16 02:16:25 +0400

    create fixed menu

--- resources/views/user/layouts/app_user.blade.php
+++ resources/views/user/layouts/app_user.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
     <title>@yield('pageTitle')</title>
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
--- resources/views/user/layouts/new_header.blade.php
+++ resources/views/user/layouts/new_header.blade.php
@@ -1,18 +1,22 @@
-<header role="banner">
-    <nav id="menubar">
-        <ul role="menubar">
-            <li role="menuitem">
-                <a href="#">о нас</a>
-            </li>
-            <li role="menuitem">
-                <a href="#">доставка</a>
-            </li>
-            <li role="menuitem">
-                <a href="#">контакты</a>
-            </li>
-            <li role="menuitem">
-                <input type="text" name="globalSearch" title="search" placeholder="Поиск по сайту">
-            </li>
-        </ul>
+<header role="banner" class="main-header">
+    <nav>
+        <div class="container">
+            <div class="d-flex justify-content-center">
+                <ul role="menubar" class="grid">
+                    <li role="menuitem">
+                        <a href="#">о нас</a>
+                    </li>
+                    <li role="menuitem">
+                        <a href="#">доставка</a>
+                    </li>
+                    <li role="menuitem">
+                        <a href="#">контакты</a>
+                    </li>
+                    <li role="menuitem">
+                        <input type="text" name="globalSearch" title="search" placeholder="Поиск по сайту">
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 </header>
