a8944f3cbe98b2d09f9ad3e94388ad20151762e8
Beluaev Vladislav <beluaev.vladislav@gmail.com>
2019-06-14 15:01:45 +0300

    continue make adaptive

--- resources/views/user/layouts/new_header.blade.php
+++ resources/views/user/layouts/new_header.blade.php
@@ -16,23 +16,28 @@
                         <a href="{{route('user.contacts')}}">контакты</a>
                     </li>
                 </ul>
-                <ul role="menubar" class="mobile-menu">
-                    <li role="menuitem">
-                        <a href="" onclick="return false;"><i class="fa fa-bars fa-2x" aria-hidden="true"></i></a>
-                    </li>
-                    <li role="menuitem">
-                        <a href="#">о нас</a>
-                    </li>
-                    <li role="menuitem">
-                        <a href="#">доставка</a>
-                    </li>
-                    <li role="menuitem">
-                        <a href="#">таблица размеров</a>
-                    </li>
-                    <li role="menuitem">
-                        <a href="{{route('user.contacts')}}">контакты</a>
-                    </li>
-                </ul>
+                <div class="mobile-menu-container">
+                    <p>
+                        <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+                    </p>
+                    <ul role="menubar" class="mobile-menu d-none">
+                       {{-- <li role="menuitem">
+                            <a href="{{route('user.index')}}">Главная</a>
+                        </li>--}}
+                        <li role="menuitem">
+                            <a href="#">о нас</a>
+                        </li>
+                        <li role="menuitem">
+                            <a href="#">доставка</a>
+                        </li>
+                        <li role="menuitem">
+                            <a href="#">таблица размеров</a>
+                        </li>
+                        <li role="menuitem">
+                            <a href="{{route('user.contacts')}}">контакты</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </nav>
@@ -40,7 +45,8 @@
         <div class="container">
             <div class="contacts-bar__content grid">
                 <div class="content__logo">
-                    <p><a href="{{route('user.index')}}"><img src="{{asset('images/logo_etalon.PNG')}}" alt="main_logo"></a></p>
+                    <p><a href="{{route('user.index')}}"><img src="{{asset('images/logo_etalon.PNG')}}" alt="main_logo"></a>
+                    </p>
                 </div>
                 <div class="content__social grid">
                     <p><a href="#"><span class="fab fa-instagram fa-2x"></span></a></p>
@@ -65,6 +71,11 @@
                             <img src="{{asset('images/catalog/man.jpg')}}" alt="man-cart">
                             <img src="{{asset('images/catalog/man_hover.jpg')}}" alt="man-cart" class="d-none">
                             <span>Мужские майки</span>
+                            <span class="mobile-content__item-span">
+                                <span>Мужские</span>
+                                <span>майки</span>
+                            </span>
+
                         </a>
                     </p>
                 </div>
@@ -73,7 +84,11 @@
                         <a href="{{route('user.woman_t_shirts')}}">
                             <img src="{{asset('images/catalog/woman.jpg')}}" alt="woman-cart">
                             <img src="{{asset('images/catalog/woman_hover.jpg')}}" alt="woman-cart" class="d-none">
-                            <span>Женские майки</span>
+                            <span>Женские майки </span>
+                            <span class="mobile-content__item-span">
+                                <span>Женские</span>
+                                <span>майки</span>
+                            </span>
                         </a>
                     </p>
                 </div>
@@ -83,6 +98,10 @@
                             <img src="{{asset('images/catalog/child.jpg')}}" alt="child-cart">
                             <img src="{{asset('images/catalog/child_hover.jpg')}}" alt="child-cart" class="d-none">
                             <span>Детские майки</span>
+                            <span class="mobile-content__item-span">
+                                <span>Детские</span>
+                                <span>майки</span>
+                            </span>
                         </a>
                     </p>
                 </div>
