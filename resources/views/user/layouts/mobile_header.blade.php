12acb09ae20423996368230cebd4be13878b2091
Beluaev Vladislav <beluaev.vladislav@gmail.com>
2019-06-18 18:04:52 +0300

    create new mobile header

--- resources/views/user/layouts/mobile_header.blade.php
+++ resources/views/user/layouts/mobile_header.blade.php
@@ -1,7 +1,7 @@
-<header role="banner" class="mobile-header">
+<header role="banner">
     <nav>
-        <div class="container">
-            <div class="d-flex justify-content-center">
+        <div class="adaptive_menu-bg">
+            <div class="container">
                 <ul role="menubar" class="grid">
                     <li role="menuitem">
                         <a href="#">о нас</a>
@@ -16,96 +16,60 @@
                         <a href="{{route('user.contacts')}}">контакты</a>
                     </li>
                 </ul>
-                <div class="mobile-menu-container">
-                    <p>
-                        <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
-                    </p>
-                    <ul role="menubar" class="mobile-menu d-none">
-                        {{-- <li role="menuitem">
-                             <a href="{{route('user.index')}}">Главная</a>
-                         </li>--}}
-                        <li role="menuitem">
-                            <a href="#">о нас</a>
-                        </li>
-                        <li role="menuitem">
-                            <a href="#">доставка</a>
-                        </li>
-                        <li role="menuitem">
-                            <a href="#">таблица размеров</a>
-                        </li>
-                        <li role="menuitem">
-                            <a href="{{route('user.contacts')}}">контакты</a>
-                        </li>
-                    </ul>
+            </div>
+        </div>
+        <div class="banner__contacts-bg">
+            <div class="container">
+                <div class="contacts-wrapper grid">
+                    <div class="contacts__logo">
+                        <p><a href="{{route('user.index')}}"><img src="{{asset('images/logo_etalon.PNG')}}" alt="main_logo"></a>
+                    </div>
+                    <div class="contacts__social grid">
+                        <p><a href="#"><span class="fab fa-instagram fa-2x"></span></a></p>
+                        <p><a href="#"><span class="fab fa-facebook fa-2x"></span></a></p>
+                        <p><a href="#"><span class="fab fa-viber fa-2x"></span></a></p>
+                        <p><a href="#"><span class="fab fa-vk fa-2x"></span></a></p>
+                    </div>
+                    {{--<div class="contacts__messengers grid">
+                        <p><a href="#"><span class="fab fa-viber fa-2x"></span></a></p>
+                        <p><a href="#"><span class="fab fa-vk fa-2x"></span></a></p>
+                    </div>--}}
+                    <div class="contacts__contact grid">
+                        <p><a href="tel:+375296038038">+375(29) 6-038-038</a></p>
+                        <p><span>график работы: пн-пт с 9 до 18</span></p>
+                        <p><a href="mailto:uzbolka@mail.ru">uzbolka@mail.ru</a></p>
+                    </div>
+                </div>
+                <div class="nav-bar grid" role="navigation">
+                    <div class="nav-bar__item">
+                        <p>
+                            <a href="{{route('user.man_t_shirts')}}">
+                                <img src="{{asset('images/catalog/man.jpg')}}" alt="man-cart">
+                                <img src="{{asset('images/catalog/man_hover.jpg')}}" alt="man-cart" class="d-none">
+                                <span>Мужские майки</span>
+                            </a>
+                        </p>
+                    </div>
+                    <div class="nav-bar__item">
+                        <p>
+                            <a href="{{route('user.woman_t_shirts')}}">
+                                <img src="{{asset('images/catalog/woman.jpg')}}" alt="woman-cart">
+                                <img src="{{asset('images/catalog/woman_hover.jpg')}}" alt="woman-cart" class="d-none">
+                                <span>Женские майки </span>
+                            </a>
+                        </p>
+                    </div>
+                    <div class="nav-bar__item">
+                        <p>
+                            <a href="{{route('user.children_t_shirts')}}">
+                                <img src="{{asset('images/catalog/child.jpg')}}" alt="child-cart">
+                                <img src="{{asset('images/catalog/child_hover.jpg')}}" alt="child-cart" class="d-none">
+                                <span>Детские майки</span>
+                            </a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     </nav>
-    <div class="banner__contacts-bar">
-        <div class="container">
-            <div class="contacts-bar__content grid">
-                <div class="content__logo">
-                    <p><a href="{{route('user.index')}}"><img src="{{asset('images/logo_etalon.PNG')}}" alt="main_logo"></a>
-                    </p>
-                </div>
-                <div class="content__social grid">
-                    <p><a href="#"><span class="fab fa-instagram fa-2x"></span></a></p>
-                    <p><a href="#"><span class="fab fa-facebook fa-2x"></span></a></p>
-                    <p><a href="#"><span class="fab fa-viber fa-2x"></span></a></p>
-                    <p><a href="#"><span class="fab fa-vk fa-2x"></span></a></p>
-                </div>
-                <div class="content__contacts grid">
-                    <p><a href="tel:+375296038038">+375(29) 6-038-038</a></p>
-                    <p><span>график работы: пн-пт с 9 до 18</span></p>
-                    <p><a href="mailto:uzbolka@mail.ru">uzbolka@mail.ru</a></p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="banner__nav-bar" role="navigation">
-        <div class="container p-2">
-            <div class="nav-bar__content grid">
-                <div class="content__item">
-                    <p>
-                        <a href="{{route('user.man_t_shirts')}}">
-                            <img src="{{asset('images/catalog/man.jpg')}}" alt="man-cart">
-                            <img src="{{asset('images/catalog/man_hover.jpg')}}" alt="man-cart" class="d-none">
-                            <span>Мужские майки</span>
-                            <span class="mobile-content__item-span">
-                                <span>Мужские</span>
-                                <span>майки</span>
-                            </span>
-
-                        </a>
-                    </p>
-                </div>
-                <div class="content__item">
-                    <p>
-                        <a href="{{route('user.woman_t_shirts')}}">
-                            <img src="{{asset('images/catalog/woman.jpg')}}" alt="woman-cart">
-                            <img src="{{asset('images/catalog/woman_hover.jpg')}}" alt="woman-cart" class="d-none">
-                            <span>Женские майки </span>
-                            <span class="mobile-content__item-span">
-                                <span>Женские</span>
-                                <span>майки</span>
-                            </span>
-                        </a>
-                    </p>
-                </div>
-                <div class="content__item">
-                    <p>
-                        <a href="{{route('user.children_t_shirts')}}">
-                            <img src="{{asset('images/catalog/child.jpg')}}" alt="child-cart">
-                            <img src="{{asset('images/catalog/child_hover.jpg')}}" alt="child-cart" class="d-none">
-                            <span>Детские майки</span>
-                            <span class="mobile-content__item-span">
-                                <span>Детские</span>
-                                <span>майки</span>
-                            </span>
-                        </a>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
 </header>
